e1bd05e0dac41e54f4b2d6561a8ac86f40994a8a
Kodake <39713700+Kodake@users.noreply.github.com>
2021-11-21 00:25:55 -0400

    All changes about RabbitMqAPI

---
+++ BackEnd/Controllers/TestRabbitMqController.cs
@@ -0,0 +1,27 @@
+﻿using BackEnd.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestRabbitMqController : Controller
+    {
+        private readonly AmqpService amqpService;
+
+        public TestRabbitMqController(AmqpService amqpService)
+        {
+            this.amqpService = amqpService ?? throw new ArgumentNullException(nameof(amqpService));
+        }
+
+        [HttpPost("")]
+        public IActionResult PublishMessage([FromBody] object message)
+        {
+            amqpService.PublishMessage(message);
+            return Ok();
+        }
+    }
+}
